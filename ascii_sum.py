8e3745b8d0179166768da27aa6a1757cd6db4f22
Desmond Chi <desmond.chi.g@gmail.com>
2024-04-18 08:52:36 -0700

    Implement CLI tool for near-duplicate detection using simhash.

--- ascii_sum.py
+++ ascii_sum.py
@@ -5,12 +5,31 @@ def ascii_sum(text: str) -> int:
   
   return sum
 
-while True:
-  text = input("Input: ")
-  a_sum = ascii_sum(text)
-  hashed_sum = a_sum % 256
+def dec_to_bin(num: int) -> str:
+  """
+  Converts decimal number into 8-bit binary string (num <= 256).
+  """
+  binary_str = bin(num)
+   
+  res = ""
 
-  print(format(1, "#04b"))
-  print(f"Ascii Sum: {a_sum}")
-  print(f"Hashed Sum: {hashed_sum}")
-  print(f"Binary String: {format(hashed_sum, "#08b")}")
+  for i in reversed(range(len(binary_str))):
+    if binary_str[i] == "b" or i == len(binary_str) - 1 - 8:
+      break
+
+    res += binary_str[i]
+  
+  while len(res) != 8:
+    res += "0"
+  
+  return res[::-1]
+
+# while True:
+#   text = input("Input: ")
+#   a_sum = ascii_sum(text)
+#   hashed_sum = a_sum % 256
+
+#   print(format(1, "#04b"))
+#   print(f"Ascii Sum: {a_sum}")
+#   print(f"Hashed Sum: {hashed_sum}")
+#   print(f"Binary String: {dec_to_bin(hashed_sum)}")
--- main.py
+++ main.py
@@ -1,3 +1,71 @@
-import ascii_sum
+from ascii_sum import ascii_sum, dec_to_bin
+from collections import Counter
+
+def hash_word(word: str) -> str:
+  asc_sum = ascii_sum(word)
+  return dec_to_bin(asc_sum % 256)
 
 doc_1 = input("Input document 1: ")
+doc_2 = input("Input document 2: ")
+print("")
+
+doc_1_words = doc_1.split(" ")
+doc_2_words = doc_2.split(" ")
+
+doc_1_word_counter = Counter()
+doc_2_word_counter = Counter()
+
+for word in doc_1_words:
+  doc_1_word_counter[word.lower()] += 1
+
+for word in doc_2_words:
+  doc_2_word_counter[word.lower()] += 1
+
+# Hash words.
+doc_1_hashed_words = {}
+doc_2_hashed_words = {}
+
+for word in doc_1_word_counter.keys():
+  doc_1_hashed_words[word] = hash_word(word)
+
+for word in doc_2_word_counter.keys():
+  doc_2_hashed_words[word] = hash_word(word)
+
+# Build weight vectors V.
+V_1 = [None for _ in range(8)]
+V_2 = [None for _ in range(8)] 
+
+for i in range(8):
+  weight_1 = 0
+  weight_2 = 0
+
+  for k, v in doc_1_hashed_words.items():
+    if v[i] == "1":
+      weight_1 += doc_1_word_counter[k]
+    else:
+      weight_1 -= doc_1_word_counter[k]
+
+  V_1[i] = weight_1
+
+  for k, v in doc_2_hashed_words.items():
+    if v[i] == "1":
+      weight_2 += doc_2_word_counter[k]
+    else:
+      weight_2 -= doc_1_word_counter[k]
+
+  V_2[i] = weight_2
+
+# Compute simhash vectors.
+simhash_1 = [(1 if n > 0 else 0) for n in V_1]
+simhash_2 = [(1 if n > 0 else 0) for n in V_2]
+
+# Compute simhash distance.
+simhash_distance = 0
+for i in range(8):
+  if simhash_1[i] != simhash_2[i]:
+    simhash_distance += 1
+
+print(f"Simhash of document 1: {simhash_1}")
+print(f"Simhash of document 2: {simhash_2}")
+print("")
+print(f"Simhash Distance (Hamming distance): {simhash_distance}")
